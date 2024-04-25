b6bbf622a4ebc822b37c0a7416956bde6f978241
Aida <aidarile@gmail.com>
2024-04-25 18:33:30 +0200

    Añado estructura de carpetas, añado primeras rutas, añado estilos globales

--- RestauranteApu/src/app/app.routes.ts
+++ RestauranteApu/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: "contacto",
+        component: ContactoComponent
+    },
+    {
+        path: "reservas",
+        component: ReservasComponent
+    }
+];
---
+++ RestauranteApu/src/app/pages/contacto/contacto.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './contacto.component.html',
+  styleUrl: './contacto.component.css'
+})
+export class ContactoComponent {
+
+}
