da1daeca9260eec06cc2496c534db2e3750be1c9
Kevin Petryla <kevin.petryla@cieep.com>
2024-04-27 14:48:19 +0200

    Avance en aboutus

--- RestauranteApu/src/app/app.component.ts
+++ RestauranteApu/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CartaComponent } from './pages/carta/carta.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContactoComponent, CartaComponent, IniciosesionComponent, RegistroComponent],
+  imports: [RouterOutlet, HeaderComponent, ContactoComponent, CartaComponent, IniciosesionComponent, RegistroComponent,AboutusComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- RestauranteApu/src/app/app.routes.ts
+++ RestauranteApu/src/app/app.routes.ts
@@ -4,24 +4,33 @@ import { ReservasComponent } from './pages/reservas/reservas.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { Component } from '@angular/core';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 export const routes: Routes = [
     { 
         path: "contacto",
+        title: "Contacto",
         component: ContactoComponent
     },
     {
         path: "reservas",
+        title: "Reservas",
         component: ReservasComponent
     },
     {
         path:"iniciodesesion",
-        title: "Inicio Sesion",
+        title: "Iniciar Sesion",
         component: IniciosesionComponent
     },
     {
         path: "registro",
         title: "Registro",
         component: RegistroComponent
+    },
+    {
+        path: "aboutus",
+        title: "Sobre nosotros",
+        component: AboutusComponent
     }
 ];
