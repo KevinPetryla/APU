db090c37488c0bd04d53598a6599dd67e086a657
Aida <aidarile@gmail.com>
2024-05-01 11:21:47 +0200

    barra de navegacion por terminar

--- RestauranteApu/src/app/app.routes.ts
+++ RestauranteApu/src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { ReservasComponent } from './pages/reservas/reservas.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
 export const routes: Routes = [
     { 
         path: "contacto",
+        title: "Contacto",
         component: ContactoComponent
     },
     {
         path: "reservas",
+        title: "Reservas",
         component: ReservasComponent
     },
     {
