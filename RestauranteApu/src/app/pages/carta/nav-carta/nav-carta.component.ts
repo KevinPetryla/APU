8a8c834def9de10beb16698144c0aa2ab892084e
ChristianRodrigo98 <christian.rodrigoalvarez@cieep.com>
2024-05-03 00:51:53 +0200

    Navbar Menu sin funcionar

---
+++ RestauranteApu/src/app/pages/carta/nav-carta/nav-carta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-carta',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-carta.component.html',
+  styleUrl: './nav-carta.component.css'
+})
+export class NavCartaComponent {
+
+}
